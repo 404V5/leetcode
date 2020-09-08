00cd2d06422284aab383c127d5ffadc6348f57be
songpb <spbdyyyy@163.com>
2020-09-08 14:57:33 +0800

    Hello World

---
+++ src/main/java/com/zhishi/leetcode/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.zhishi.leetcode;
+
+/**
+ * Created by songpb on 2020/9/8.
+ * Hello World !
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world !");
+    }
+}
