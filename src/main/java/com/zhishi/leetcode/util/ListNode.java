a5f403a85bc4d1f134f0c6e8fff603f487385272
songpb <spbdyyyy@163.com>
2020-10-04 20:41:54 +0800

    两数相加2

---
+++ src/main/java/com/zhishi/leetcode/util/ListNode.java
@@ -0,0 +1,22 @@
+package com.zhishi.leetcode.util;
+
+/**
+ * Created by songpb on 2020/10/4.
+ * 工具类
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
