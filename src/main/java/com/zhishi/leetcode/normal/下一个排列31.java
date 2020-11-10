b414956ff6f986826dddf68e8c64a7c45ed1d578
songpb <spbdyyyy@163.com>
2020-11-10 13:17:40 +0800

    下一个排列31

---
+++ src/main/java/com/zhishi/leetcode/normal/下一个排列31.java
@@ -0,0 +1,40 @@
+package com.zhishi.leetcode.normal;
+
+/**
+ * Created by songpb on 2020/11/10.
+ * https://leetcode-cn.com/problems/next-permutation/
+ */
+public class 下一个排列31 {
+    /**
+     * 方法一：两遍扫描
+     */
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
