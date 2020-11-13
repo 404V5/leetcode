169d4591f8456e742666351d08ea0255d32e066f
songpb <spbdyyyy@163.com>
2020-11-14 00:06:49 +0800

    数组的相对排序1122

---
+++ src/main/java/com/zhishi/leetcode/normal/数组的相对排序1122.java
@@ -0,0 +1,35 @@
+package com.zhishi.leetcode.normal;
+
+/**
+ * Created by songpb on 2020/11/14.
+ * https://leetcode-cn.com/problems/relative-sort-array/
+ */
+public class 数组的相对排序1122 {
+    /**
+     * 方法一：计数排序
+     */
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int upper = 0;
+        for (int x : arr1) {
+            upper = Math.max(upper, x);
+        }
+        int[] frequency = new int[upper + 1];
+        for (int x : arr1) {
+            ++frequency[x];
+        }
+        int[] ans = new int[arr1.length];
+        int index = 0;
+        for (int x : arr2) {
+            for (int i = 0; i < frequency[x]; ++i) {
+                ans[index++] = x;
+            }
+            frequency[x] = 0;
+        }
+        for (int x = 0; x <= upper; ++x) {
+            for (int i = 0; i < frequency[x]; ++i) {
+                ans[index++] = x;
+            }
+        }
+        return ans;
+    }
+}
