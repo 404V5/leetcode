a4658d5fa1194d2f943b73debeb95cb00415f22a
songpb <spbdyyyy@163.com>
2020-11-15 18:08:42 +0800

    移掉K位数字402

---
+++ src/main/java/com/zhishi/leetcode/normal/移掉K位数字402.java
@@ -0,0 +1,43 @@
+package com.zhishi.leetcode.normal;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * Created by songpb on 2020/11/15.
+ * https://leetcode-cn.com/problems/remove-k-digits/
+ */
+public class 移掉K位数字402 {
+    /**
+     * 方法一：贪心 + 单调栈
+     */
+    public String removeKdigits(String num, int k) {
+        Deque<Character> deque = new LinkedList<Character>();
+        int length = num.length();
+        for (int i = 0; i < length; ++i) {
+            char digit = num.charAt(i);
+            while (!deque.isEmpty() && k > 0 && deque.peekLast() > digit) {
+                deque.pollLast();
+                k--;
+            }
+            deque.offerLast(digit);
+        }
+
+        for (int i = 0; i < k; ++i) {
+            deque.pollLast();
+        }
+
+        StringBuilder ret = new StringBuilder();
+        boolean leadingZero = true;
+        while (!deque.isEmpty()) {
+            char digit = deque.pollFirst();
+            if (leadingZero && digit == '0') {
+                continue;
+            }
+            leadingZero = false;
+            ret.append(digit);
+        }
+        return ret.length() == 0 ? "0" : ret.toString();
+    }
+}
+
