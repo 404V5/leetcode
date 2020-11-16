77a73229863e3971fe680b4788085168c2c22c71
songpb <spbdyyyy@163.com>
2020-11-17 00:10:15 +0800

    距离顺序排列矩阵单元格1030

---
+++ src/main/java/com/zhishi/leetcode/normal/距离顺序排列矩阵单元格1030.java
@@ -0,0 +1,85 @@
+package com.zhishi.leetcode.normal;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Created by songpb on 2020/11/17.
+ * https://leetcode-cn.com/problems/matrix-cells-in-distance-order/
+ */
+public class 距离顺序排列矩阵单元格1030 {
+    /**
+     * 方法一：直接排序
+     */
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int[][] ret = new int[R * C][];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                ret[i * C + j] = new int[]{i, j};
+            }
+        }
+        Arrays.sort(ret, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return (Math.abs(a[0] - r0) + Math.abs(a[1] - c0)) - (Math.abs(b[0] - r0) + Math.abs(b[1] - c0));
+            }
+        });
+        return ret;
+    }
+
+    /**
+     * 方法二：桶排序
+     */
+//    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+//        int maxDist = Math.max(r0, R - 1 - r0) + Math.max(c0, C - 1 - c0);
+//        List<List<int[]>> bucket = new ArrayList<List<int[]>>();
+//        for (int i = 0; i <= maxDist; i++) {
+//            bucket.add(new ArrayList<int[]>());
+//        }
+//
+//        for (int i = 0; i < R; i++) {
+//            for (int j = 0; j < C; j++) {
+//                int d = dist(i, j, r0, c0);
+//                bucket.get(d).add(new int[]{i, j});
+//            }
+//        }
+//        int[][] ret = new int[R * C][];
+//        int index = 0;
+//        for (int i = 0; i <= maxDist; i++) {
+//            for (int[] it : bucket.get(i)) {
+//                ret[index++] = it;
+//            }
+//        }
+//        return ret;
+//    }
+//
+//    public int dist(int r1, int c1, int r2, int c2) {
+//        return Math.abs(r1 - r2) + Math.abs(c1 - c2);
+//    }
+
+    /**
+     * 方法三：几何法    int[] dr = {1, 1, -1, -1};
+     //    int[] dc = {1, -1, -1, 1};
+     //
+     //    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+     //        int maxDist = Math.max(r0, R - 1 - r0) + Math.max(c0, C - 1 - c0);
+     //        int[][] ret = new int[R * C][];
+     //        int row = r0, col = c0;
+     //        int index = 0;
+     //        ret[index++] = new int[]{row, col};
+     //        for (int dist = 1; dist <= maxDist; dist++) {
+     //            row--;
+     //            for (int i = 0; i < 4; i++) {
+     //                while ((i % 2 == 0 && row != r0) || (i % 2 != 0 && col != c0)) {
+     //                    if (row >= 0 && row < R && col >= 0 && col < C) {
+     //                        ret[index++] = new int[]{row, col};
+     //                    }
+     //                    row += dr[i];
+     //                    col += dc[i];
+     //                }
+     //            }
+     //        }
+     //        return ret;
+     //    }
+     */
+//
+}
