4100f0463d409cbddd1378773166c17a093ea270
songpb <spbdyyyy@163.com>
2020-11-24 18:04:17 +0800

    完全二叉树的节点个数222

---
+++ src/main/java/com/zhishi/leetcode/normal/完全二叉树的节点个数222.java
@@ -0,0 +1,49 @@
+package com.zhishi.leetcode.normal;
+
+import com.zhishi.leetcode.util.TreeNode;
+
+/**
+ * Created by songpb on 2020/11/24.
+ * https://leetcode-cn.com/problems/count-complete-tree-nodes/
+ */
+public class 完全二叉树的节点个数222 {
+    /**
+     * 方法一：二分查找 + 位运算
+     */
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int level = 0;
+        TreeNode node = root;
+        while (node.left != null) {
+            level++;
+            node = node.left;
+        }
+        int low = 1 << level, high = (1 << (level + 1)) - 1;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (exists(root, level, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public boolean exists(TreeNode root, int level, int k) {
+        int bits = 1 << (level - 1);
+        TreeNode node = root;
+        while (node != null && bits > 0) {
+            if ((bits & k) == 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+            bits >>= 1;
+        }
+        return node != null;
+    }
+
+}
