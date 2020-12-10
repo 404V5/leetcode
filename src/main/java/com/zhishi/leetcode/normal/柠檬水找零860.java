c3c5c26e555a592cd7421a317cb7aa3775cf2723
songpb <spbdyyyy@163.com>
2020-12-10 19:26:39 +0800

    柠檬水找零860

---
+++ src/main/java/com/zhishi/leetcode/normal/柠檬水找零860.java
@@ -0,0 +1,32 @@
+package com.zhishi.leetcode.normal;
+
+/**
+ * Created by songpb on 2020/12/10.
+ * https://leetcode-cn.com/problems/lemonade-change/
+ */
+public class 柠檬水找零860 {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five == 0) {
+                    return false;
+                }
+                five--;
+                ten++;
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
