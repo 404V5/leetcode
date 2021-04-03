ca76cc2594cd14d8a9be24393131a24535879213
songpb <spbdyyyy@163.com>
2021-04-04 02:11:49 +0800

    设计模式之模板模式

---
+++ src/main/java/com/zhishi/designpattern/template/TemplateMethodPatternDemo.java
@@ -0,0 +1,53 @@
+package com.zhishi.designpattern.template;
+
+public class TemplateMethodPatternDemo {
+    public static void main(String[] args) {
+        DiscountCalculator calculator1 = new DiscountCalculator1();
+        calculator1.calculate();
+        DiscountCalculator calculator2 = new DiscountCalculator2();
+        calculator2.calculate();
+        DiscountCalculator calculator3 = new DiscountCalculator3();
+        calculator3.calculate();
+    }
+
+    public interface DiscountCalculator{
+        void calculate();
+    }
+
+    /**
+     * 模板方法实现的精华所在
+     */
+    public static abstract class AbstractDiscountCalculator implements DiscountCalculator{
+        public void calculate(){
+            //完成通用的计算逻辑
+            commomCalculate();
+            //完成特殊的计算逻辑
+            specificCalculate();
+        }
+
+        private void commomCalculate(){
+//            System.out.println("通用的计算逻辑");
+            System.out.println("通用的计算逻辑修改了一下");
+        }
+
+        protected abstract void specificCalculate();
+    }
+
+    public static class DiscountCalculator1 extends AbstractDiscountCalculator{
+        public void specificCalculate(){
+            System.out.println("优惠计算器1的特殊计算逻辑");
+        }
+    }
+
+    public static class DiscountCalculator2 extends AbstractDiscountCalculator{
+        public void specificCalculate(){
+            System.out.println("优惠计算器2的特殊计算逻辑");
+        }
+    }
+
+    public static class DiscountCalculator3 extends AbstractDiscountCalculator{
+        public void specificCalculate(){
+            System.out.println("优惠计算器3的特殊计算逻辑");
+        }
+    }
+}
